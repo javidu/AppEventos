b1cd56f467fe288b98f92bc719b2541cab25a74e
FRANCISCO JAVIER DUARTE GARCÍA <javinovich_12@hotmail.com>
2016-10-18 17:18:14 -0500

    Creacion conexion

--- src/java/com/appeventos/DAO/DAOEmprendedor.java
+++ src/java/com/appeventos/DAO/DAOEmprendedor.java
@@ -6,15 +6,30 @@
 package com.appeventos.DAO;
 
 import com.appeventos.DTO.Emprendedor;
+import com.appeventos.Util.ConexionMysql;
 
 /**
  *
  * @author JAVIER
  */
-public class DAOEmprendedor {
+public class DAOEmprendedor extends ConexionMysql{
     
+    public DAOEmprendedor(){
+        
+    }
     
+    /* 
+     * METODO CON RETORNO BOOLEANO QUE RELIZA LA FUNCIONALIDAD DE REGISTRAR UN NUEVO EMPRENDEDOR EN LA BASE DE DATOS
+     * RETORNA VERDADERO (TRUE) SI SE REALIZO EL REGISTRO 
+     * RETORNA FALSE (FALSE) SI NO SE PUDO REALIZAR EL REGISTRO
+     */
     public void RegistrarEmprendedor(Emprendedor e){
         
     }
+    
+    public boolean validarEmprendedor(){
+        
+        return false;
+    }
+    
 }
---
+++ src/java/com/appeventos/Util/Conexion.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appeventos.Util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author JAVIER
+ */
+public class Conexion {
+
+    private String db = "jdbc:mysql://sandbox2.ufps.edu.co/ufps_63";
+    private String user = "ufps_63";
+    private String clave = "ufps_82";
+    private Connection con;
+
+    public Conexion() {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection(db, user, clave);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+
+    }
+
+    public Connection getCon() {
+        return con;
+    }
+
+}
